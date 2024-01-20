02bf75f99d0a0e1c236fd7803240e77e25cd3603
neroblack <39099894+neroblack4life@users.noreply.github.com>
2024-01-20 07:53:36 +0100

    updated the new main.tf  with netsork and IP resource

--- quickstart/101-windows-vm-with-iis-server/Testing-Folders/main.tf
+++ quickstart/101-windows-vm-with-iis-server/Testing-Folders/main.tf
@@ -8,8 +8,7 @@ terraform {
         random = {
             source = "hashicorp/random"
             version = "~>3.0"
-        }
-        
+        }  
     }
 }
 provider azurerm {
@@ -24,7 +23,7 @@ variable "resouce_group_location" {
 variable "prefix" {
     type = string
     default = "windows-IIS-VM"
-    decription = "Prefix of the resource name"
+    description = "Prefix of the resource name"
 }
 
 resource "random_pet" "prefix" {
@@ -37,7 +36,8 @@ resource "random_password" {
    min_lower   = 1
    min_upper   = 1
    min_numeric = 1
-   min_special = 1 
+   min_special = 1
+   special = true 
 }
 
 resource "random_id" {
@@ -48,6 +48,35 @@ resource "random_id" {
 }
 
 resource "azurerm_resource_group" "rg" {
-    name = "${random_pet.prefix.id}-rg"
-    location = var.resource_group_location
+    name                  = "${random_pet.prefix.id}-rg"
+    location              = var.resource_group_location
+}
+
+# Create Virtual network
+resource "azurerm_virtual_network" "my_terraform_network" {
+    name                  = "${random_pet.prefix.id}-vnet"
+    resource_group_name   = azurerm_resource_group.rg.name
+    address_space         = "[10.0.0.0/16]"
+    location              = azurerm_resource_group.rg.location
+}
+
+# Create a Subnet
+resource "azurerm_subnet" "my_terraform_subnet" {
+    name                  = "${random_pet.prefix.id}-subnet"
+    resource_group_name   = azurerm_resource_group.rg.name
+    virtual_network_name  = azurerm_virtual_network.my_terraform_network.name
+    address_prefixes      = "[10.0.1.0/24]"
+}
+
+# Create Public IP
+resource "azurerm_public_ip" "my_terraform_public_ip" {
+    name                  = "${random_pet.prefix.id}-public-ip"
+    location              = azurerm_resource_group.rg.location
+    resource_group_name   = azurerm_resource_group.rg.name
+    allocation_method     = "Dynamic"
+}
+
+# Create NSG & Rules
+resource "" "" {
+    
 }
